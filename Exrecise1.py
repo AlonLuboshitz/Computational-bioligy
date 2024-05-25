f4de2cd1f660b0fc2ea17a43ac21ae235ccb9c14
AlonLuboshitz <alibi2322@gmail.com>
2024-05-25 19:07:58 +0300

    asdas

--- Exrecise1.py
+++ Exrecise1.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 import random
-
+from collections import Counter
 labels = []
 labels_colors_before = []
 labels_colors_after = []
-rows = 10
-columns = 10
+rows = 50
+columns = 50
 color_dict ={"black": "white", "white": "black"}
  
 def create_board():
@@ -18,7 +18,7 @@ def create_board():
             color = random.choices(["black", "white"], weights=[0.5, 0.5])[0]
             color_row.append(color)
             label = tk.Label(root, bg=color, borderwidth=1, relief="solid", width=8, height=2)
-            label.grid(row=row, column=col, padx=5, pady=5)
+            label.grid(row=row, column=col)
             row_labels.append(label)
         labels.append(row_labels)
         labels_colors_before.append(color_row)
@@ -28,20 +28,27 @@ def get_nieghbours(i,j):
     '''functiong ets i,j returns vector with colors of the 8 niegerbours clock wise
     ----- 
     returns color_vec = [(i-1,j-1).....(i,j-1)]'''
-    color_vec = []
+    color_vec_column = []
+    
+    color_voc_rows = []
     # check if the nieghbours are in the board
     if i > 0 and i < (rows-1) and j > 0 and j < (columns-1):
-        color_vec.append(labels_colors_before[i-1][j-1])
-        color_vec.append(labels_colors_before[i-1][j])
-        color_vec.append(labels_colors_before[i-1][j+1])
-        color_vec.append(labels_colors_before[i][j+1])
-        color_vec.append(labels_colors_before[i+1][j+1])
-        color_vec.append(labels_colors_before[i+1][j])
-        color_vec.append(labels_colors_before[i+1][j-1])
-        color_vec.append(labels_colors_before[i][j-1])
-        return color_vec
-    return None
-
+        color_vec_column.append(labels_colors_before[i+1][j])
+        color_vec_column.append(labels_colors_before[i-1][j])
+        color_voc_rows.append(labels_colors_before[i-1][j-1])
+        color_voc_rows.append(labels_colors_before[i-1][j+1])
+        color_voc_rows.append(labels_colors_before[i][j+1])
+        color_voc_rows.append(labels_colors_before[i+1][j+1])
+        color_voc_rows.append(labels_colors_before[i+1][j-1])
+        color_voc_rows.append(labels_colors_before[i][j-1])
+        return color_vec_column,color_voc_rows
+    return 0,0
+def color_count_column(column_vec,row_vec):
+    counter_column = Counter(column_vec)
+    counter_row = Counter(row_vec)
+    black = 0.4*(counter_column["black"]/2) + 0.6*(counter_row["white"]/6)
+    color = random.choices(["black", "white"], weights=[black, 1-black])[0]
+    return color  
 def change_color_by_column(current_color,nieghbours_vec):
     '''Function to change the node color by the niehbrous in the colmuns'''
     colors = []
@@ -54,9 +61,9 @@ def inverse_colors():
         color_row = []
         for j,label in enumerate(row):
             color = label.cget("bg")
-            neighrous_vec = get_nieghbours(i,j)
-            if neighrous_vec:
-                inverse_color = change_color_by_column(color,neighrous_vec)
+            col_vec,row_vec = get_nieghbours(i,j)
+            if col_vec and row_vec:
+                inverse_color = color_count_column(col_vec,row_vec)
                 label.config(bg=inverse_color)
     root.update()
     switch_color_labels()
@@ -74,7 +81,7 @@ def switch_color_labels():
 #     return None
 def recu_inverse():
     inverse_colors()
-    root.after(100, recu_inverse)
+    root.after(1, recu_inverse)
 def on_button_click(row, col):
     print(f"Button clicked at ({row}, {col})")
 
@@ -82,8 +89,9 @@ def on_button_click(row, col):
 root = tk.Tk()
 root.title("10x10 Board")
 create_board()
-recu_inverse()
-
+#recu_inverse()
+for i in range(250):
+    inverse_colors()
 
 root.mainloop()
 
