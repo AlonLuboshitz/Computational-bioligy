a12f27934ab06ab43492e3db29169f16ad968657
AlonLuboshitz <alibi2322@gmail.com>
2024-07-03 19:27:42 +0300

    adas

--- EX3/hexa.py
+++ EX3/hexa.py
@@ -25,8 +25,7 @@ def get_closest_neuron(x, matrix):
     Returns:
     The flat index of the closest neuron'''
     
-    if len(x) != matrix.shape[1]:
-        raise ValueError('Input vector x has wrong dimension')
+    
     # Calculate the Euclidean distances between x and each row of A_reshaped
     distances = np.linalg.norm(matrix - x, axis=2)
     # Find the flat index of the row with the minimum distance
@@ -42,16 +41,73 @@ def init_x_prototypes(X, n_neurons):
     -----------
     Returns: x_nerons np.array (n_samples,) with the neuron labels'''
     return np.random.randint(0, n_neurons, size=X.shape[0])
+def get_learning_rates(net_num):
+    prototype_learning_rates = np.random.choice(np.linspace(0.2, 0.8, net_num), net_num)
+    neighbor_learning_rates = np.random.choice(np.linspace(0.05, 0.2, net_num), net_num)
+    neighbor_learning_rate_level_2 = np.random.choice(np.linspace(0.01, 0.1, net_num), net_num)
+    # combine 8 learning rates
+    learning_rates = list(zip(prototype_learning_rates[:8], neighbor_learning_rates[:8], neighbor_learning_rate_level_2[:8]))
+    # Add 2 learning rate where prototype has lower learning rate
+    learning_rates.extend(zip(neighbor_learning_rate_level_2[8:], neighbor_learning_rates[8:], prototype_learning_rates[8:]))
+    return learning_rates
+def best_net_search(net_num, n_cols = 10, n_rows = 10):
+    '''Create net_number of learning rates combinations and run each one.
+    for each combination save the plot/ topological score/ distance score.'''
+    X = pd.read_csv("EX3/digits_test.csv")
+    X = np.array(X)
+    label_df = pd.read_csv("EX3/digits_keys.csv", header=None, names=["label"]) # labels
+    label_indices = get_labels_indexes(label_df) # Get index for each label for further comparison
+    learning_rates = get_learning_rates(net_num)
+    with open("scores_values_hex.txt", "w") as f:
+        for i, rates in enumerate(learning_rates):
+            outputpath = f"{i+1}_iteration.png"
+            print(f"Running net number {i+1} out of {net_num}")
+            x_prototypes = init_x_prototypes(X, n_cols * n_rows)
+            x_prototypes,matrix = main(label_df,X,x_prototypes,outputpath,rates,n_cols, n_rows)
+            topo_scores = topological_score(X,label_indices, matrix)
+            f.write(f"Net number {i+1}:\nTotal topo score: {sum(topo_scores)}\nTopo scores: {topo_scores}\nLearning_rates: {rates}\n\n")
+            print(f"Finished net number {i+1} out of {net_num}")
+def get_labels_indexes(label_df):
+    '''Get 1 index from each uniue label in the label_df'''
+    unique_labels = label_df['label'].unique()
+    unique_labels.sort()
+    label_indexes = []
+    for label in unique_labels:
+        label_indexes.append(label_df[label_df['label'] == label].index[0])
+    return label_indexes
+def get_2_closest_neuron(x, matrix):
+    '''Function iterate the matrix and return the 2 neurons closest to the input x
+    Args:
+    1. x: input vector
+    2. matrix: matrix of neurons (each row is a neuron, each column is a feature)
+    -----------
+    Returns:
+    The flat index of the closest neuron'''
+    distances = np.linalg.norm(matrix - x, axis=2)
+    # Find the flat index of the row with the minimum distance
+    flat_indices = np.argsort(distances, axis=None)
+    first_min = np.unravel_index(flat_indices[0], distances.shape)
+    second_min = np.unravel_index(flat_indices[1], distances.shape)
+    return first_min, second_min 
+def topological_score(X,label_inidces, matrix):
+    topo_scores = []
+    for indices in label_inidces:
+        x = X[indices]
+        first_min,second_min = get_2_closest_neuron(x, matrix)
+        distance = hex_distance(first_min, second_min)
+        topo_scores.append(distance)
+    return topo_scores
 
-def calculate_distances(X, x_neurons, matrix):
-    ''''The function calculates the distance between each input vector and the neuron'''
-    n_sqrt = matrix.shape[0]
-    matrix = transfrom_to_nd(matrix)
-    for x_index, x_vector in enumerate(X):
-        min_neuron_index = get_closest_neuron(x_vector, matrix, n_sqrt)
-        x_neurons[x_index] = min_neuron_index
-    return x_neurons
+def axial_to_cube(q, r):
+    x = q
+    z = r
+    y = -x - z
+    return x, y, z
 
+def hex_distance(a, b):
+    x1, y1, z1 = axial_to_cube(*a)
+    x2, y2, z2 = axial_to_cube(*b)
+    return max(abs(x1 - x2), abs(y1 - y2), abs(z1 - z2))
 class HexagonalMatrix:
     def __init__(self, rows, cols, pixels):
         self.rows = rows
@@ -60,7 +116,7 @@ class HexagonalMatrix:
         self.time_passed = 0
         self.matrix = np.zeros((rows, cols,pixels))
 
-    def fit_x(self,X, x_prototypes, sparse_perc = 0.8, levels=2, max_iter = 100, max_time=180):
+    def fit_x(self,X, x_prototypes,rates, sparse_perc = 0.8, levels=2, max_iter = 100, max_time=180):
         '''Function to "train" the matrix to fit the input data.
         Args:
         1. X: np.array (n_samples, d_features)
@@ -79,8 +135,7 @@ class HexagonalMatrix:
                 min_neuron_index = get_closest_neuron(x_vector, self.matrix)
                 x_prototypes[x_index] = min_neuron_index
                 i,j = np.unravel_index(min_neuron_index, (self.rows, self.cols))
-
-                learning_rates = [0.2,0.1,0.05]
+                learning_rates = rates
                 # update nuerons:
                 self.update_prototype(i, j, x_vector, learning_rates[0])
 
@@ -193,16 +248,18 @@ class HexagonalMatrix:
         # final_neigbors = neighbors[level] - old_neighbors
         # return final_neigbors
     
-    def display_matrix(self, x_prototypes, label_df):
+    def display_matrix(self, x_prototypes, label_df,output_path):
         fig, ax = plt.subplots(1, figsize=(10, 10))
         ax.set_aspect('equal')
         
         # Get unique labels and assign colors
         unique_labels = label_df['label'].unique()
-        
-        color_palette = sns.color_palette("hsv", len(unique_labels))
+        unique_labels.sort()
+        color_palette = sns.color_palette("bright", len(unique_labels))
         label_to_color = {label: color for label, color in zip(unique_labels, color_palette)}
 
+        legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=label) for label, color in label_to_color.items()]
+        
         # Define hexagon size
         hex_size = 1
         for row in range(self.rows):
@@ -219,17 +276,20 @@ class HexagonalMatrix:
                     most_frequent_label, percentage = value
                     # Adjust color based on percentage
                     base_color = label_to_color[most_frequent_label]
-                    #alpha = percentage / 100  # Scale percentage to [0, 1] for alpha
-                    rgba_color = base_color#, alpha)
+                    alpha = percentage / 100  # Scale percentage to [0, 1] for alpha
+                    rgba_color = (*base_color, alpha)
                     ax.text(x, y, f'{most_frequent_label}\n{percentage:.1f}%', ha='center', va='center', size=6)
                     hexagon.set_facecolor(rgba_color)
+                    
                 else:
                     hexagon.set_facecolor('gray')  # Default color for empty cells
         
+        ax.legend(handles = legend_handles)
         plt.xlim(-hex_size, self.cols * hex_size * 3/2)
         plt.ylim(-hex_size, self.rows * np.sqrt(3) * hex_size)
         plt.axis('off')
-        plt.show()
+        #plt.show()
+        plt.savefig(output_path)
 
 def get_neuron_label(nueron_index, prototypes, labels):
     prototypes = pd.DataFrame(prototypes, columns=["prototypes"])
@@ -247,16 +307,19 @@ def get_neuron_label(nueron_index, prototypes, labels):
 
 
 
-def main(n_cols = 10,n_rows = 10):
-    X = pd.read_csv("EX3/digits_test.csv")
-    X = np.array(X)
-    x_prototypes = init_x_prototypes(X, n_cols * n_rows)
+def main(label_df,X,x_prototypes,output_path,rates,n_cols = 10,n_rows = 10):
+    # X = pd.read_csv("EX3/digits_test.csv")
+    # X = np.array(X)
+    # x_prototypes = init_x_prototypes(X, n_cols * n_rows)
     hex_matrix = HexagonalMatrix(n_rows,n_cols,784)
-    label_df = pd.read_csv("EX3/digits_keys.csv", header=None, names=["label"]) # labels
-    x_prototypes = hex_matrix.fit_x(X, x_prototypes,sparse_perc =0.8, levels=2, max_iter=30, max_time=60)
-    hex_matrix.display_matrix(x_prototypes,label_df)
+    # label_df = pd.read_csv("EX3/digits_keys.csv", header=None, names=["label"]) # labels
+    x_prototypes = hex_matrix.fit_x(X, x_prototypes,rates,sparse_perc =0.8, levels=2, max_iter=100, max_time=90)
+    hex_matrix.display_matrix(x_prototypes,label_df,output_path)
+    return x_prototypes,hex_matrix.get_matrix()
 
 
 if __name__ == "__main__":
-    main()
+    
+    best_net_search(10)
+    
 
